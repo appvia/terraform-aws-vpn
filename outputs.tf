9b69efdb621b79d65c5cfb8bf611f74b077f10c2
Rohith Jayawardene <gambol99@gmail.com>
2024-04-24 18:46:18 +0100

    chore: adding additional outputs to the module

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,17 @@
 
 output "vpn_endpoint_dns_name" {
   description = "The DNS Name of the Client VPN Endpoint Connection."
-  value       = var.enable_vpn ? module.client_vpn[0].client_vpn_endpoint_dns_name : null
+  value       = var.enable_vpn ? module.client_vpn[0].vpn_endpoint_dns_name : null
+}
+
+output "vpn_endpoint_arn" {
+  description = "The ARN of the Client VPN Endpoint Connection."
+  value       = var.enable_vpn ? module.client_vpn[0].vpn_endpoint_arn : null
+}
+
+output "vpn_endpoint_id" {
+  description = "The ID of the Client VPN Endpoint Connection."
+  value       = var.enable_vpn ? module.client_vpn[0].vpn_endpoint_id : null
 }
 
 output "client_configuration" {
